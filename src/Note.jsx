29eacbb3d323276d882a9082f4aed58a7870e6bc
johnbriancapate <johnbriancapate@gmail.com>
2022-12-06 21:22:12 +0800

    first commit

---
+++ src/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>For the title</h1>
+      <p> for the content</p>
+    </div>
+  );
+}
+
+export default Note;
